5bc1c784e4da1e22f7d8ff58325cf62c126eb9e9
PAKAPON CHAIMONGKOLTRAKUL <pakapon.chaimongkoltrakul@siemens.com>
2026-03-08 19:50:11 +0700

    change model to models

--- alembic/env.py
+++ alembic/env.py
@@ -7,7 +7,7 @@ from sqlalchemy.ext.asyncio import async_engine_from_config
 
 from alembic import context
 import sys
-from sqlmodel import SQLModel
+from model.models import SQLModel
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
